5d69dbfe2d681615ab1206181d5e6a947bfe28f9
syausv <syausv@gmail.com>
2023-05-10 21:27:18 +0300

    Lesson 24. Do planets

--- main.js
+++ main.js
@@ -1,207 +1,60 @@
-import 'uno.css';
-import '@unocss/reset/tailwind.css';
-import 'toastify-js/src/toastify.css';
-import DOM from './src/constants/dom';
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-import { delay } from './src/utils/timeUtils.js';
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-import Toastify from 'toastify-js';
-
-import TasksModel from './src/mvc/model/TasksModel.js';
-import TasksController from './src/mvc/controller/TasksController.js';
-import NetworkService from './src/service/NetworkService.js';
-
-const KEY_LOCAL_TASKS = 'tasks';
-
-const Tags = ['Web', 'Update', 'Design', 'Content'];
-
-const getDOM = (id) => document.getElementById(id);
-const QUERY = (container, id) => container.querySelector(`[data-id="${id}"]`);
-
-const domTemplateTask = getDOM(DOM.Template.TASK);
-const domTaskColumn = domTemplateTask.parentNode;
-
-const networkService = new NetworkService('http://localhost:3000');
-const tasksModel = new TasksModel();
-const tasksController = new TasksController(tasksModel, networkService);
-
-domTemplateTask.removeAttribute('id');
-domTemplateTask.remove();
-
-function renderTask(taskVO) {
-  const domTaskClone = domTemplateTask.cloneNode(true);
-  domTaskClone.dataset.id = taskVO.id;
-  QUERY(domTaskClone, DOM.Template.Task.TITLE).innerText = taskVO.title;
-  domTaskColumn.prepend(domTaskClone);
-  return domTaskClone;
-}
-
-const showToastWithText = (text) =>
-  Toastify({
-    text,
-    duration: 3000,
-    close: true,
-  }).showToast();
-
-async function main() {
-  tasksModel.addUpdateCallback((tasks) => {
-    console.log('> addUpdateCallback: ', tasks);
-    domTaskColumn.innerHTML = '';
-    tasks.forEach((taskVO) => renderTask(taskVO));
-  });
-  tasksController
-    .retrieveTasks()
-    .then(() => {
-
-    })
-    .catch((e) => {});
-
-  const taskOperations = {
-    [DOM.Button.CREATE_TASK]: () => {
-      renderTaskPopup(
-        null,
-        'Create task',
-        'Create',
-        (taskTitle, taskDate, taskTags) => {
-          console.log('> Create task -> On Confirm');
-          tasksController
-            .createTask(taskTitle, taskDate, taskTags)
-            .then((taskVO) => {
-              console.log('> Create task -> On Confirm: Success');
-              showToastWithText(`You task saved: ${taskVO.title}`);
-            })
-            .catch((error) => {
-              console.log('> Create task -> On Confirm: Error =', error);
-              window.alert(`Error on server: ${error.toString()}`);
-            });
-        }
-      );
-    },
-    [DOM.Template.Task.BTN_DELETE]: (taskId) => {
-      const taskVO = tasksModel.getTaskById(taskId);
-      renderTaskPopup(
-        taskVO,
-        'Confirm delete task?',
-        'Delete',
-        (taskTitle, taskDate, taskTag) => {
-          console.log('> Delete task -> On Confirm', {
-            taskTitle,
-            taskDate,
-            taskTag,
-          });
-          tasksController
-            .deleteTask(taskId)
-            .then(() => {
-              showToastWithText(`Task deleted: ${taskVO.title}`);
-            })
-            .catch((e) => {});
-        }
-      );
-    },
-    [DOM.Template.Task.BTN_EDIT]: (taskId) => {
-      const taskVO = tasksModel.getTaskById(taskId);
-      renderTaskPopup(
-        taskVO,
-        'Update task',
-        'Update',
-        (taskTitle, taskDate, taskTag) => {
-          console.log('> Update task -> On Confirm', {
-            taskTitle,
-            taskDate,
-            taskTag,
-          });
-          tasksController.updateTaskById(taskId,taskTitle, taskDate, taskTag);
-        }
-      );
-    },
-  };
-
-  domTaskColumn.onclick = (e) => {
-    e.stopPropagation();
-    console.log('domTaskColumn', e.target);
-    const domTaskElement = e.target;
-    const taskBtn = domTaskElement.dataset.btn;
-
-    const isNotTaskBtn = !taskBtn;
-    if (isNotTaskBtn) return;
-
-    const allowedButtons = [
-      DOM.Template.Task.BTN_EDIT,
-      DOM.Template.Task.BTN_DELETE,
-    ];
-    if (!allowedButtons.includes(taskBtn)) return;
-
-    let taskId;
-    let domTask = domTaskElement;
-    do {
-      domTask = domTask.parentNode;
-      taskId = domTask.dataset.id;
-    } while (!taskId);
-
-    const taskOperation = taskOperations[taskBtn];
-    if (taskOperation) taskOperation(taskId);
-  };
-
-  getDOM(DOM.Button.CREATE_TASK).addEventListener('click', (e) =>
-    taskOperations[DOM.Button.CREATE_TASK]()
-  );
-
-  async function renderTaskPopup(
-    taskVO,
-    popupTitle,
-    confirmText,
-    processDataCallback
-  ) {
-    const domPopupContainer = getDOM(DOM.Popup.CONTAINER);
-    const domSpinner = domPopupContainer.querySelector('.spinner');
-
-    domPopupContainer.classList.remove('hidden');
-
-    const onClosePopup = () => {
-      document.onkeyup = null;
-      domPopupContainer.children[0].remove();
-      domPopupContainer.append(domSpinner);
-      domPopupContainer.classList.add('hidden');
-    };
-
-    const TaskPopup = (await import('./src/mvc/view/popup/TaskPopup')).default;
-    const taskPopupInstance = new TaskPopup(
-      popupTitle,
-      Tags,
-      confirmText,
-      (taskTitle, taskDate, taskTags) => {
-        console.log('Main -> renderTaskPopup: confirmCallback', {
-          taskTitle,
-          taskDate,
-          taskTags,
-        });
-        processDataCallback(taskTitle, taskDate, taskTags);
-        onClosePopup();
-      },
-      onClosePopup
-    );
-
-    if (taskVO) {
-      taskPopupInstance.taskTitle = taskVO.title;
-    }
-
-    delay(1000).then(() => {
-      console.log('render 1');
-      domSpinner.remove();
-      document.onkeyup = (e) => {
-        if (e.key === 'Escape') {
-          onClosePopup();
-        }
-      };
-      domPopupContainer.append(taskPopupInstance.render());
-    });
-
-    console.log('render 0');
+class Planet {
+  x;
+  y;
+  size;
+  pX;
+  pY;
+  atmosphere;
+  radius;
+  alpha;
+  constructor(x, y, size = 10, atmosphere = 'red', radius = 50) {
+    this.size = size;
+    this.pX = x;
+    this.pY = y;
+    this.atmosphere = atmosphere;
+    this.radius = radius;
+    this.alpha = 0;
   }
 
-  function saveTask() {
-    localStorage.setItem(KEY_LOCAL_TASKS, JSON.stringify(tasks));
+  move() {
+    this.x = this.radius * Math.sin(this.alpha) + this.pX;
+    this.y = this.radius * Math.cos(this.alpha) + this.pY;
+    this.alpha += (0.1 * Math.PI) / 180;
+  }
+
+  render(ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = this.atmosphere;
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
   }
 }
 
-main();
+const planets = [
+  new Planet(200, 200, 30, 'red', 50),
+  new Planet(200, 200, 10, 'blue', 100),
+  new Planet(200, 200, 20, 'green', 150),
+  new Planet(200, 200, 50, 'gold', 200),
+];
+
+let planet;
+
+const render = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (const planetIndex in planets) {
+    planet = planets[planetIndex];
+    planet.move();
+    planet.render(ctx);
+  }
+  window.requestAnimationFrame(render);
+};
+
+window.requestAnimationFrame(render);
