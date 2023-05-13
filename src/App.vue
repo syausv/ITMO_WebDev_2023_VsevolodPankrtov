36025521f8104ef155d0edec095e2cbb8b7dab3f
syausv <syausv@gmail.com>
2023-05-13 09:49:32 +0300

    Basic MyName component with events and passing status in props

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import MyName from "./components/MyName.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <select name='status'
+          @change='onSelectChange' >
+    <option v-if='!status'
+            value='=unselected'
+            selected>
+      Unselected
+    </option>
+  <option
+    v-for="item in ['Ready', 'Start', 'Stop']"
+    :key="item"
+    :value="item"
+    :selected="item === status"
+  >
+    {{ item }}
+  </option>
+  </select>
+  <MyName :status='status' @reset='onStatusReset'/>
 </template>
 
+<script>
+export default {
+  data: () => ({
+    status: "Loading"
+  }),
+  methods:{
+    onSelectChange(event){
+      console.log(">App -> onSelectChange",event.target.value);
+      this.status = event.target.value;
+    },
+    onStatusReset(){
+      console.log(">App -> onStatusReset");
+      this.status = null;
+
+    }
+  }
+
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- src/components/MyName.vue
+++ src/components/MyName.vue
@@ -1,13 +1,55 @@
 <template>
-$END$
+  <h1>Enter you name</h1>
+  <div>
+    {{ firstName }}
+    <span style="color: red;">
+      {{ lastName }}
+    </span>
+  </div>
+  <small>
+    <div>
+      <span class='flex flex-col'
+            :class="{'selected': !!status,
+             'unselected': !status} "
+      >Status:</span>
+      <span >{{ status }}</span>
+    </div>
+    <div>
+      <button
+        :style='{visibility: !!status}' @click='onResetClick'
+      >Reset</button>
+    </div>
+  </small>
 </template>
 
 <script>
 export default {
-name: "MyName"
-}
+  name: "MyName",
+  props: {
+    status:{
+      type: String,
+      default: "-"
+    }
+  },
+  emits:['reset'],
+  data: () => ({
+    firstName: "Vsevolod",
+    lastName: "Pankratov"
+  }),
+  methods: {
+    onResetClick(){
+      console.log(' MyName -> onResetClick');
+      this.$emit('reset');
+    }
+  }
+};
 </script>
 
 <style scoped>
-
+.selected {
+color: green;
+}
+.unselected{
+  color: red;
+}
 </style>
