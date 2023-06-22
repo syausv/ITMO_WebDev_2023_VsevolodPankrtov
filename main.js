5cf7e00687f76a1a1b142563d1af650bd3ca9db5
syausv <syausv@gmail.com>
2023-06-22 17:24:22 +0300

    Homework 5. Do html calculator + operations

--- main.js
+++ main.js
@@ -1,135 +1,57 @@
-import Dom from './src/constants/Dom';
+document.getElementById('resultCalc').readOnly = true;
 
 
-const KEY_LOCAL_ITEM = 'item';
 
-class ItemVO {
-  static fromJSON(json) {
-    return new ItemVO(json.id, json.title, json.description, json.qty, json.cost, json.total);
-  }
-  constructor( id, title, description, qty, cost, total) {
-    this.id = id;
-    this.title = title;
-    this.description = description;
-    this.qty = qty;
-    this.cost = cost;
-    this.total = total;
-  }
+let btnResult = document.querySelector('#btnResult');
+let btnMinus = document.querySelector('#btnMinus');
+let btnDel = document.querySelector('#btnDel');
+let btnPlus = document.querySelector('#btnPlus');
+let btnUmn = document.querySelector('#btnUmn');
+
+let result;
+
+
+
+
+btnMinus.onclick = () => {
+ let firstNumber = document.getElementById('firstNumber').value;
+ let secondNumber = document.getElementById('secondNumber').value;
+ result = firstNumber - secondNumber;
+ return result;
 }
 
-const getDOM = (id) => document.getElementById(id);
-const QUERY = (container, id) => container.querySelector(`[data-id='${id}']`);
-
-
-
-const domTemplateItem = getDOM(Dom.Template.ITEM)
-const domItemColumn = domTemplateItem.parentNode;
-domTemplateItem.removeAttribute('id');
-domTemplateItem.remove();
-
-const rawItem = localStorage.getItem(KEY_LOCAL_ITEM);
-
-const items = rawItem ? JSON.parse(rawItem).map((json) => ItemVO.fromJSON(json)) : [] ;
-items.forEach((itemVO) => renderItem(itemVO));
-console.log('> items', items);
-
-domItemColumn.onclick = (e) => {
-  e.stopPropagation();
-  console.log(e.target);
-  const domItemElement = e.target;
-  const itemId = domItemElement.dataset.id;
-  if (!itemId) return;
-
-
-  const itemVO = items.find((item) => item.id === itemId);
-  console.log("> itemVO", itemVO);
-  renderItemPopup(itemVO,
-    'Update',
-    'Save',
-    (itemTitle, itemDescription,
-     itemQty, itemCost, itemTotal) => {
-    console.log('> Update item -> On confirm',{itemTitle, itemDescription,
-        itemQty, itemCost, itemTotal});
-    itemVO.title = itemTitle;
-    const domItemUpdated = renderItem(itemVO);
-    domItemColumn.replaceChild(domItemUpdated,domItemElement);
-    saveItem();
-  });
+btnDel.onclick = () => {
+ let firstNumber = document.getElementById('firstNumber').value;
+ let secondNumber = document.getElementById('secondNumber').value;
+ result = firstNumber / secondNumber;
+ return result;
 }
 
-getDOM(Dom.Button.CREATE_ITEM).onclick = () => {
-  console.log('> domPopupCreateItem.classList');
-  renderItemPopup(null,
-    'Add',
-    'Create',
-    (itemTitle, itemDescription, itemQty,
-     itemCost, itemTotal) => {
-    console.log('> Create item -> On confirm');
-    const itemId = `task_${items.length}_${Date.now()}`;
-    const itemVO = new ItemVO(itemId, itemTitle, itemDescription,
-      itemQty, itemCost, itemTotal);
-
-    renderItem(itemVO);
-    items.push(itemVO);
-
-    saveItem();
-  });
-};
-
-function renderItem (itemVO) {
-  const domItemClone = domTemplateItem.cloneNode(true);
-  domItemClone.dataset.id = itemVO.id;
-
-  QUERY(domItemClone, Dom.Template.Item.TITLE).innerText = itemVO.title;
-  QUERY(domItemClone, Dom.Template.Item.DESCRIPTION).innerText = itemVO.description;
-  QUERY(domItemClone, Dom.Template.Item.QTY).innerText = itemVO.qty;
-  QUERY(domItemClone, Dom.Template.Item.COST).innerText = itemVO.cost;
-  QUERY(domItemClone, Dom.Template.Item.TOTAL).innerText = itemVO.total;
-
-  domItemColumn.prepend(domItemClone);
-  return domItemClone;
+btnPlus.onclick = () => {
+ let firstNumber = document.getElementById('firstNumber').value;
+ let secondNumber = document.getElementById('secondNumber').value;
+ firstNumber =  Number(firstNumber);
+ secondNumber =  Number(secondNumber);
+ result = firstNumber + secondNumber;
+ console.log("result",result);
+ return result;
 }
 
-async function renderItemPopup(
-  itemVO,
-  popupTitle,
-  confirmText,
-  processDataCallback
-)  {
-  const domPopupContainer = getDOM(Dom.Popup.CONTAINER);
- console.log("classList", domPopupContainer)
-  domPopupContainer.classList.remove('hidden');
-
-  const onClosePopup = () => {
-    domPopupContainer.children[0].remove();
-    domPopupContainer.classList.add('hidden');
-  };
-
-const ItemPopup = (await import('./src/view/popup/ItemPopup')).default;
-const itemPopupInstance = new ItemPopup(
-  popupTitle,
-  confirmText,
-  (itemTitle, itemDescription, itemQty, itemCost, itemTotal) =>{
-    console.log('Main -> processDataCallback',{itemTitle, itemDescription, itemQty, itemCost, itemTotal});
-    processDataCallback(itemTitle, itemDescription, itemQty, itemCost, itemTotal)
-    onClosePopup();
-  },
-  onClosePopup
-);
-
-if (itemVO) {
-  itemPopupInstance.itemTitle = itemVO.title;
+btnUmn.onclick = () => {
+ let firstNumber = document.getElementById('firstNumber').value;
+ let secondNumber = document.getElementById('secondNumber').value;
+ result = firstNumber * secondNumber;
+ return result;
 }
 
- // document.onkeyup = (e) => {
- //   if (e.key === 'Escape') {
- //     onClosePopup();
-  //  }
- // };
-console.log("itemPopupInstance",itemPopupInstance);
-domPopupContainer.append(itemPopupInstance.render());
+
+btnResult.onclick = () => {
+ console.log("result",result);
 }
 
-function saveItem() {
-  localStorage.setItem(KEY_LOCAL_ITEM,JSON.stringify(items));
-}
+
+
+
+
+
+
